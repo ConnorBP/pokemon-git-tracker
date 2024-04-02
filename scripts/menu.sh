c24e473bcbc208ceb0738a2e2f56ba5d1efea5a9
Restyled.io <commits@restyled.io>
2024-04-02 17:12:49 +0000

    Restyled by shellcheck

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -29,7 +29,7 @@
      M5(){ TPUT  9 20; $e "ABOUT  ";}
      M6(){ TPUT 10 20; $e "EXIT   ";}
       LM=6
-   MENU(){ for each in $(seq 0 $LM);do M${each};done;}
+   MENU(){ for each in $(seq 0 $LM);do M"${each}";done;}
     POS(){ if [[ $cur == up ]];then ((i--));fi
            if [[ $cur == dn ]];then ((i++));fi
            if [[ $i -lt 0   ]];then i=$LM;fi
@@ -41,7 +41,7 @@ REFRESH(){ after=$((i+1)); before=$((i-1))
            if [[ $after -eq 0 ]] || [ $before -eq $LM ];then
            UNMARK; M$before; M$after;fi;j=$i;UNMARK;M$before;M$after;}
    INIT(){ R;HEAD;FOOT;MENU;}
-     SC(){ REFRESH;MARK;$S;$b;cur=`ARROW`;}
+     SC(){ REFRESH;MARK;$S;$b;cur=$(ARROW);}
      ES(){ MARK;$e "ENTER = main menu ";$b;read;INIT;};INIT
   while [[ "$O" != " " ]]; do case $i in
         0) S=M0;SC;if [[ $cur == "" ]];then R;$e "\n$(w        )\n";ES;fi;;
